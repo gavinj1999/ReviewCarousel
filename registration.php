0b7c88f6f4d54b64a4d99044f625d308f1a12cbc
gavinj1999 <gavinj2006@googlemail.com>
2025-08-09 18:21:46 +0100

    new composer.json

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'ReviewCarousel',
+    'Rising5th_ReviewCarousel',
     __DIR__
 );
